89541dfe41ea1aa4354942663cbb803343e605ad
jjgene <108653152+jjgene@users.noreply.github.com>
2022-11-03 21:37:56 +0900

    dependency 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import "antd/dist/antd.css";
 import React from "react";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
