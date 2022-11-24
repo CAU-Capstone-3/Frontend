8265cb49a0ac4824867e966c435bd421d3ebac43
JangHoonSeok <orirori512@naver.com>
2022-11-25 04:46:57 +0900

    ♻️ refactor: 일단 동작하게 다시 돌려놓음

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import NotFound from "./pages/NotFound";
+import SubjectTopicPage from "./pages/SubjectTopicPage";
+import TopicNotePage from "./pages/TopicNotePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import WritePage from "./pages/WritePage";
+import TopicBoard from "./pages/TopicBoard";
+import New from "./pages/New";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "topic/subject/:subjectId", element: <SubjectTopicPage /> }, //과목별토픽목록
+      { path: "note/topic/:topicId", element: <TopicNotePage /> }, //토픽별노트목록
+      { path: "note/:noteId", element: <NoteDetailPage /> }, //노트상세조회
+      { path: "note", element: <WritePage /> }, //글작성페이지
+      { path: "topic", element: <TopicBoard /> }, //토픽모음
+      { path: "advice", element: <New /> }, //문장분석페이지
+    ],
+  },
+]);
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
