13eac9eb023c6a2353c61f7e482eeaba5a0fdc4c
jjgene <108653152+jjgene@users.noreply.github.com>
2022-11-24 10:26:08 +0900

    라우터 수정중

--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import NotFound from "./pages/NotFound";
-import SubjectTopicPage from "./pages/SubjectTopicPage";
-import TopicNotePage from "./pages/TopicNotePage";
-import NoteDetailPage from "./pages/NoteDetailPage";
-import WritePage from "./pages/WritePage";
-import TopicBoard from "./pages/TopicBoard";
-import New from "./pages/New";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      { path: "topic/subject/:subjectId", element: <SubjectTopicPage /> }, //과목별토픽목록
-      { path: "note/topic/:topicId", element: <TopicNotePage /> }, //토픽별노트목록
-      { path: "note/:noteId", element: <NoteDetailPage /> }, //노트상세조회
-      { path: "note", element: <WritePage /> }, //글작성페이지
-      { path: "topic", element: <TopicBoard /> }, //토픽모음
-      { path: "advice", element: <New /> }, //문장분석페이지
-    ],
-  },
-]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
