d5bff95ca5fb772a6ba208f5ea4647cef6c78410
jjgene <108653152+jjgene@users.noreply.github.com>
2022-11-24 11:23:40 +0900

    과목별토픽목록

---
+++ src/pages/SubjectTopic.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Main from "./Main";
+/**과목별토픽목록**/
+const SubjectTopic = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Main />
+    </div>
+  );
+};
+
+export default SubjectTopic;
